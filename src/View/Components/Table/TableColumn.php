eca174f83969e2a78ae475e40e9633ab32fd87df
tovitch <tovitch@icloud.com>
2021-01-04 11:13:46 +0100

    Allow table row's render method to return a string

--- src/View/Components/Table/TableColumn.php
+++ src/View/Components/Table/TableColumn.php
@@ -2,6 +2,9 @@
 
 namespace Tovitch\BladeUI\View\Components\Table;
 
+use Illuminate\Container\Container;
+use Illuminate\Contracts\Support\Htmlable;
+use Illuminate\Contracts\View\View as ViewContract;
 use Illuminate\View\Component;
 use Illuminate\Support\HtmlString;
 use Illuminate\Support\Facades\App;
@@ -61,7 +64,50 @@ class TableColumn extends Component
     {
         $instance = App::make($this->component, ['component' => $this]);
 
-        return $instance->render($row);
+        $view = $instance->render($row);
+
+        if ($view instanceof ViewContract) {
+            return $view;
+        }
+
+        if ($view instanceof Htmlable) {
+            return $view;
+        }
+
+        $resolver = function ($view) {
+            $factory = Container::getInstance()->make('view');
+
+            return $factory->exists($view)
+                ? $view
+                : $this->createBladeViewFromString($factory, $view);
+        };
+
+        return view($resolver($view))->with('row', $row);
+    }
+
+    /**
+     * Create a Blade view with the raw component string content.
+     *
+     * @param  \Illuminate\Contracts\View\Factory  $factory
+     * @param  string  $contents
+     * @return string
+     */
+    protected function createBladeViewFromString($factory, $contents)
+    {
+        $factory->addNamespace(
+            '__components',
+            $directory = Container::getInstance()['config']->get('view.compiled')
+        );
+
+        if (! is_file($viewFile = $directory.'/'.sha1($contents).'.blade.php')) {
+            if (! is_dir($directory)) {
+                mkdir($directory, 0755, true);
+            }
+
+            file_put_contents($viewFile, $contents);
+        }
+
+        return '__components::'.basename($viewFile, '.blade.php');
     }
 
     /**
